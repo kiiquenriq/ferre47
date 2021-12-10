607c52745b5d69b866b15dc793af38284418eeec
Enrique kiiquenriq <enriqcruz884@gmail.com>
2021-12-10 12:33:46 -0600

    url provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace App\Providers;
-
-use Illuminate\Contracts\Routing\UrlGenerator as RoutingUrlGenerator;
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,11 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceSchema('https');
-        }
+        URL::forceScheme('https');
     }
 }
